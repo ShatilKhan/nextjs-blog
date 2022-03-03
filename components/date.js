fe09c2a2f7ac42e88222f04fed3a9f5feaa964df
ShatilKhan <shatilshahriar009@gmail.com>
2022-03-04 03:52:04 +0600

    fixed date

--- components/date.js
+++ components/date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  return <time dateTime={dateString}>{format(parseISO('2022-01-02T14:00:00'), 'MM/dd/yyyy')}</time>
 }
